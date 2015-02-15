c4feaf24703e07ad3fdf3f451809a322ee9b9b45
X54329 <bobojoe108@gmail.com>
2015-02-15 02:14:20 -0500

    Fixed conflict

--- parser.py
+++ parser.py
@@ -5,7 +5,7 @@ import re
 string = sys.argv[1]
 #print string
 prepositions = (["of","from","to","over"])
-regex = "(of)|(from)|(to)|(over)" # shut up
+regex = "(of )|(from )|(to )|(over )" # shut up
 
 for s in filter(None, re.split(regex, string)):
 	print s
