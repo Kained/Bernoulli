b503df66241c00a4edd3558fa2d97a085e44922e
Amit Mizrahi <amitmizrahi10@gmail.com>
2015-02-15 03:37:58 -0500

    integrals work

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ app = Flask(__name__)
 import json
 
 import parser
+import clean
 
 @app.route('/')
 def hello_world():
@@ -12,7 +13,7 @@ def hello_world():
 @app.route('/parse', methods=['POST'])
 def parse():
 	text = request.form["text"]
-	return json.dumps({'output': parser.parse(text)})
+	return json.dumps({'output': parser.parse(clean.clean(text))})
 
 if __name__ == '__main__':
 	app.run()
--- clean.py
+++ clean.py
@@ -1,2 +1,16 @@
 def clean(text):
+
+	text = text.lower()
+
+	dictionary = {"andover": "n over", 
+	"roots": "root", 
+	"square root": "squareroot",
+	"end": "n",
+	"are": "r",
+	"each": "e"}
+
 	text = text.replace("andover", "n over")
+	text = text.replace("roots", "root")
+	text = text.replace("square root", "squareroot")
+
+	return text
